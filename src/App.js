36c11c936d9b09dce036225219d0486b3c420857
calebmutisya <mutisyacalebmusau@gmail.com>
2024-01-07 14:13:16 +0300

    Hero Complete

--- src/App.js
+++ src/App.js
@@ -14,7 +14,9 @@ export default function App() {
   return (
     <main className='relative'>
       <Nav/>
-      <Hero/>
+      <section className='xl:padding-l wide:padding-r padding-b'>
+        <Hero/>
+      </section>
       <Popularproducts/>
       <Superquality/>
       <Services/>
